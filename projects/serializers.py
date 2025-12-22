5867acaba4c2cdb9424541f12ca32c181434d329
Noah-droid <noahtochukwu10@gmail.com>
2025-12-22 13:52:03 +0100

    feat: added project serializers and schemas

---
+++ projects/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "name",
+            "description",
+            "environment_variables",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+
+
+
+
+
