eb76eb864fb2e0ed94796e7f1b93f41df29b26c9
Noah-droid <noahtochukwu10@gmail.com>
2025-12-25 13:40:58 +0100

    feat: added storage limits per projects

--- projects/models.py
+++ projects/models.py
@@ -13,9 +13,44 @@ class Project(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
+    # Storage quota tracking
+    storage_used_bytes = models.BigIntegerField(default=0, help_text="Total storage used in bytes")
+    storage_quota_bytes = models.BigIntegerField(default=104_857_600, help_text="Storage quota in bytes (default: 100 MB)")
+    
     def __str__(self):
         return self.name
+    
+    @property
+    def storage_used_mb(self):
+        """Get storage used in MB."""
+        return self.storage_used_bytes / (1024 * 1024)
+    
+    @property
+    def storage_quota_mb(self):
+        """Get storage quota in MB."""
+        return self.storage_quota_bytes / (1024 * 1024)
+    
+    @property
+    def storage_percentage_used(self):
+        """Get percentage of storage quota used."""
+        if self.storage_quota_bytes == 0:
+            return 0
+        return (self.storage_used_bytes / self.storage_quota_bytes) * 100
+    
+    def can_upload(self, file_size_bytes):
+        """Check if user has enough quota to upload a file."""
+        return (self.storage_used_bytes + file_size_bytes) <= self.storage_quota_bytes
+    
+    def increment_storage(self, bytes_added):
+        """Increment storage usage."""
+        self.storage_used_bytes += bytes_added
+        self.save(update_fields=['storage_used_bytes'])
+    
+    def decrement_storage(self, bytes_removed):
+        """Decrement storage usage (when deleting files)."""
+        self.storage_used_bytes = max(0, self.storage_used_bytes - bytes_removed)
+        self.save(update_fields=['storage_used_bytes'])
 
 
 
