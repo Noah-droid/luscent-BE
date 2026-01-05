9628df62b361ee7a0c15195c4815b76dc9708634
Noah-droid <noahtochukwu10@gmail.com>
2026-01-05 14:22:48 +0100

    fix: updated collection list endpoint

--- collection/urls.py
+++ collection/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("projects/<int:project_id>/", CollectionListCreateView.as_view(), name="collection-list-create"),
+    path("projects/<uuid:project_id>/", CollectionListCreateView.as_view(), name="collection-list-create"),
     path("<int:pk>/", CollectionDetailView.as_view(), name="collection-detail"),
     path("projects/<int:project_id>/import-swagger/", SwaggerImportView.as_view(), name="swagger-import"),
     path("projects/<int:project_id>/import-crawler/", CrawlerImportView.as_view(), name="crawler-import"),
