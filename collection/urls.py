46ca4f1aa337df4d512568fcee0dc4979d0e4ee2
Noah-droid <noahtochukwu10@gmail.com>
2026-01-08 10:25:09 +0100

    fix: added suffix to collection creation path

--- collection/urls.py
+++ collection/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("projects/<uuid:project_id>/", CollectionListCreateView.as_view(), name="collection-list-create"),
+    path("projects/<uuid:project_id>/manual-import/", CollectionListCreateView.as_view(), name="collection-list-create"),
     path("<int:pk>/", CollectionDetailView.as_view(), name="collection-detail"),
     path("projects/<uuid:project_id>/import-swagger/", SwaggerImportView.as_view(), name="swagger-import"),
     path("projects/<int:project_id>/import-crawler/", CrawlerImportView.as_view(), name="crawler-import"),
