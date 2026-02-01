ccadb1ff16767974ddb751b9f55ebb9269c7a65d
Noah-droid <noahtochukwu10@gmail.com>
2026-02-01 13:35:20 +0100

    feat: add collection endpoints

--- collection/urls.py
+++ collection/urls.py
@@ -2,14 +2,22 @@ from django.urls import path
 from .views import (
     CollectionListCreateView, 
     CollectionDetailView, 
+    EndpointListCreateView,
+    EndpointDetailView,
     SwaggerImportView,
     CrawlerImportView
 )
 
 urlpatterns = [
-    path("projects/<uuid:project_id>/manual-import/", CollectionListCreateView.as_view(), name="collection-list-create"),
+    # Collection management
+    path("projects/<uuid:project_id>/", CollectionListCreateView.as_view(), name="collection-list-create"),
     path("<int:pk>/", CollectionDetailView.as_view(), name="collection-detail"),
+    
+    # Endpoint management within collections
+    path("<int:collection_id>/endpoints/", EndpointListCreateView.as_view(), name="endpoint-list-create"),
+    path("endpoints/<int:pk>/", EndpointDetailView.as_view(), name="endpoint-detail"),
+    
+    # Bulk Imports
     path("projects/<uuid:project_id>/import-swagger/", SwaggerImportView.as_view(), name="swagger-import"),
-    path("projects/<int:project_id>/import-crawler/", CrawlerImportView.as_view(), name="crawler-import"),
+    path("projects/<uuid:project_id>/import-crawler/", CrawlerImportView.as_view(), name="crawler-import"),
 ]
-
