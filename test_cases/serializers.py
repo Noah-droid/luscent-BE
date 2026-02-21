af486f7c50d195b62c5bef88821decfc73c1d20b
Noah-droid <noahtochukwu10@gmail.com>
2026-02-21 02:45:25 +0100

    feat: populate serializers with test run and project aggregation metrics

--- test_cases/serializers.py
+++ test_cases/serializers.py
@@ -7,6 +7,11 @@ class TestCaseSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class TestRunSerializer(serializers.ModelSerializer):
+    test_case_name = serializers.ReadOnlyField(source='test_case.name')
+    endpoint_name = serializers.ReadOnlyField(source='test_case.endpoint.name')
+    endpoint_method = serializers.ReadOnlyField(source='test_case.endpoint.method')
+    endpoint_url = serializers.ReadOnlyField(source='test_case.endpoint.url')
+
     class Meta:
         model = TestRun
         fields = "__all__"
