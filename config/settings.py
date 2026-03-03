9a2dc2fad97b7ff891b08d773680bf2e25e9805e
Noah-droid <noahtochukwu10@gmail.com>
2026-03-03 17:47:26 +0100

    feat: add django-db-logger for logging

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'drf_yasg',  
 
+    'django_db_logger',
+
 
     # Local apps
     'users',
@@ -288,3 +290,35 @@ QAI_RUNNER_SECRET=config('QAI_RUNNER_SECRET')
 E2B_API_KEY = config('E2B_API_KEY', default=None)
 # The default template for E2B sandboxes
 E2B_SANDBOX_TEMPLATE = config('E2B_SANDBOX_TEMPLATE', default='base')
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
+        'db_log': {
+            'level': 'DEBUG',
+            'class': 'django_db_logger.db_log_handler.DatabaseLogHandler'
+        },
+    },
+    'loggers': {
+        'db': {
+            'handlers': ['db_log'],
+            'level': 'DEBUG'
+        },
+        'django.request': { # logging 500 errors to database
+            'handlers': ['db_log'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
